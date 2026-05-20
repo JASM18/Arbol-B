ccdca0fb41bf851df0240a76a4dea4070f637e91
Axel <jaxelsm18@gmail.com>
2026-05-20 02:12:23 -0700

    first commit

---
+++ Arbol B.cpp
@@ -0,0 +1,26 @@
+#include "Arbol B.hpp"
+
+ArbolB::ArbolB()
+{
+    //ctor
+}
+
+ArbolB::~ArbolB()
+{
+    //dtor
+}
+
+ArbolB::ArbolB(const ArbolB& other)
+{
+    //copy ctor
+}
+
+ArbolB& ArbolB::operator=(const ArbolB& arbol)
+{
+    if (this == &arbol) return *this; // handle self assignment
+    //assignment operator
+
+    // codigo...
+
+    return *this;
+}
