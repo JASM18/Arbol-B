f79ec31c7b50d4915cced38eb25b37077d113aaa
Axel <jaxelsm18@gmail.com>
2026-05-23 21:12:40 -0700

    defDatosAleatorios

---
+++ Menu.hpp
@@ -0,0 +1,10 @@
+#ifndef MENU_HPP_INCLUDED
+#define MENU_HPP_INCLUDED
+
+#include "ArbolB"
+
+// numDatos es la cantidad de datos aleatorios que va a meter
+template <typename T>
+void defDatosAleatorios(ArbolB &arbolito, int numDatos);
+
+#endif // MENU_HPP_INCLUDED
